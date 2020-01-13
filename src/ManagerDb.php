2b75ced5ea96051bff82d59e9288113acdf9957e
Artem <OleynikAP@yandex.ru>
2020-01-13 22:51:56 +0700

    fix

--- src/ManagerDb.php
+++ src/ManagerDb.php
@@ -45,11 +45,9 @@ class ManagerDb implements ManagerDbInterface
     protected function __construct($db_host, $db_name, $db_user, $db_pass)
     {
         try {
-         //   $db_host = 'localhost';
             self::$pdo = new \PDO('mysql:host=' . $db_host .';dbname=' . $db_name . ';charset=utf8', $db_user, $db_pass, [\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION]);
         } catch (\PDOException $e) {
             //TODO log
-            //echo 'Error BD';
             $this->error_messages= ['msg' => 'Error BD' ] ;
             return null;
         }
